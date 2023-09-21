5bd4fe1d38af5aa9e40bb4b643b27a44b7583bf8
2damb <2damb@informatica.edu>
2023-09-21 19:49:56 +0100

    second commit

--- src/aedapp/AED.java
+++ src/aedapp/AED.java
@@ -5,6 +5,8 @@ import personas.Estudiante;
 import personas.Empleado;
 import personas.Cliente;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -19,26 +21,39 @@ public class AED {
         Estudiante[] estudiantes = new Estudiante[3];
         Empleado[] empleados = new Empleado[3];
         Facultativo[] facultativos = new Facultativo[3];
-
+        
+        Pattern caracteresEspecialesPatron = Pattern.compile("[\"@#$%&.,*()_+=|<>?{}\\[\\]~-]");
+        Matcher specialFinder;  
+        Pattern numerosPatron = Pattern.compile("[0-9]");
+        
         String nombre;
         int codigo;
         System.out.println("AHORA VAS A INTRODUCIR EL NOMBRE DE 3 CLIENTES");
-
+        
+        Boolean nombreCorrecto=false;
+        Boolean codigoCorrecto=false;
+        
         for (int i = 0; i < clientes.length; i++) {
-
+            
+            do{
             System.out.print("Introduce el nombre: ");
             nombre = sc.nextLine();
+            specialFinder = caracteresEspecialesPatron.matcher(nombre);
+            nombreCorrecto = specialFinder.find();
+            System.out.println(nombreCorrecto);
+            }while(nombreCorrecto);
             
-            System.out.print("Introduce un numero como codigo entre 100 y 300 ambos incluidos: ");
+            /*System.out.print("Introduce un numero como codigo entre 100 y 300 ambos incluidos: ");
             codigo = sc.nextInt();
             
             clientes[i] = new Cliente(nombre,codigo);
             
             System.out.println(clientes[i].getCodigo()+" y "+ clientes[i].getNombre());
-            
+            */
             sc.nextLine();
             System.out.println("------------------------------------");
 
         }
+       
     }
 }
